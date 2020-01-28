572f5926c1985611ddf4a81bbdf41e7273b32067
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-01-28 02:27:23 +0000

    Added symbolic consts for the type of objects to use in algorithms

--- seldump/consts.py
+++ seldump/consts.py
@@ -7,23 +7,29 @@ This file is part of pg_seldump.
 VERSION = "0.1"
 PROJECT_URL = "https://github.com/dvarrazzo/pg_seldump"
 
+# kinds with a state to be dumped
+# other objects are either internal (indexes, toast tables) or stateless (views)
+DUMPABLE_KINDS = set("rSmp")
+
+# We don't care enough about the other objects to give them a symbolic const
+TABLE = 'table'
+PART_TABLE = 'partitioned table'
+MATVIEW = 'materialized view'
+SEQUENCE = 'sequence'
+
 # relkind values: https://www.postgresql.org/docs/11/catalog-pg-class.html
 PG_KINDS = {
-    "r": "table",
+    "r": TABLE,
     "i": "index",
-    "S": "sequence",
+    "S": SEQUENCE,
     "t": "toast table`",
     "v": "view",
-    "m": "materialized view",
+    "m": MATVIEW,
     "c": "composite type",
     "f": "foreign table",
-    "p": "partitioned table",
+    "p": PART_TABLE,
     "I": "partitioned index",
 }
 
 # reverse map from kind name to relkind
 REVKINDS = {v: k for k, v in PG_KINDS.items()}
-
-# kinds with a state to be dumped
-# other objects are either internal (indexes, toast tables) or stateless (views)
-DUMPABLE_KINDS = set("rSmp")
