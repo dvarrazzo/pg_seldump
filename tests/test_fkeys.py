fed659fb58d5fa677e329c90d96dd9112c7d03aa
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-02-29 20:58:01 +0000

    Added basic tables navigation along foreign keys

--- tests/test_fkeys.py
+++ tests/test_fkeys.py
@@ -13,3 +13,47 @@ def test_fkey_nav(dumper):
         obj for obj, rule in dumper.writer.dumped if isinstance(obj, Table)
     ]
     assert len(objs) == 2
+
+
+def test_fkey_nav_rec(dumper):
+    dumper.reader.load_db(
+        create_sample_db(
+            3,
+            fkeys=[
+                ("table1", "t2id", "table2", "id"),
+                ("table2", "t3id", "table3", "id"),
+            ],
+        )
+    )
+    dumper.add_config({"db_objects": [{"name": "table1"}]})
+    dumper.perform_dump()
+    objs = [
+        obj for obj, rule in dumper.writer.dumped if isinstance(obj, Table)
+    ]
+    assert len(objs) == 3
+
+
+def test_fkey_nav_stops_on_skip(dumper):
+    dumper.reader.load_db(
+        create_sample_db(
+            4,
+            fkeys=[
+                ("table1", "t2id", "table2", "id"),
+                ("table2", "t3id", "table3", "id"),
+                ("table3", "t4id", "table4", "id"),
+            ],
+        )
+    )
+    dumper.add_config(
+        {
+            "db_objects": [
+                {"name": "table1"},
+                {"name": "table3", "action": "skip"},
+            ]
+        }
+    )
+    dumper.perform_dump()
+    objs = [
+        obj for obj, rule in dumper.writer.dumped if isinstance(obj, Table)
+    ]
+    assert len(objs) == 2
